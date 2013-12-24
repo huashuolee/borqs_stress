0353a09e90e08e6ea9525b583257f6b6340f5231
Huashuo Lee <huashuolee@gmail.com>
2013-12-24 17:07:29 +0800

    python script

---
+++ falcon/LaunchExitCamera.py
@@ -0,0 +1,27 @@
+# Imports the monkeyrunner modules used by this program
+from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
+from time import sleep
+
+# Connects to the current device, returning a MonkeyDevice object
+device = MonkeyRunner.waitForConnection()
+
+
+# sets a variable with the package's internal name
+package = 'com.android.gallery3d'
+
+# sets a variable with the name of an Activity in the package
+activity = 'com.android.camera.CameraLauncher'
+
+sleep(2)
+# sets the name of the component to start
+runComponent = package + '/' + activity
+
+# Runs the component
+device.startActivity(component=runComponent)
+for i in range(0, int(raw_input('please input times:\n'))):
+    device.press('KEYCODE_CAMERA')
+    sleep(2)
+
+device.press('KEYCODE_BACK')
+print 'work done'
+
