dab85acb85a1e1f791c2fe9deb52457b26c00517
huashuolee <huashuolee@gmail.com>
2016-02-03 10:12:38 +0800

    refine autoCreateAHref.py

--- easyTools/autoCreateAHref.py
+++ easyTools/autoCreateAHref.py
@@ -3,22 +3,11 @@ import sys
 
 
 def addToFile(path):
-    filename = os.listdir(path)
-    
-    with open("filelist.txt", "w+") as f:
-    
+    filename = os.listdir(path)    
+    with open("filelist.txt", "w+") as f:    
         for item in filename:
             if item[-3:] == "mp4" or item[-3:]=="mkv" or item[-3:] == "mvb":
-                f.write(' <a href= " ' + sys.argv[1] + "\\")
-                f.write(item)
-                f.write(' "> ')
-                f.write(item)
-                f.write(' </a>')
-                f.write(" </br>")
-                f.write("\n")
-            
-
-            
-            
+                f.write("<a href=%s\\%s >%s</a> </br>\n"%(sys.argv[1],item,item))
+                
 if __name__ == "__main__":
     addToFile(sys.argv[1])
