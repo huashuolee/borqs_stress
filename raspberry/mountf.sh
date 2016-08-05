91d3d567258c117f6fd96def0e43b4e50fee8356
huashuolee <huashuolee@gmail.com>
2016-08-05 11:38:37 +0800

    raspberry

---
+++ raspberry/mountf.sh
@@ -0,0 +1,1 @@
+mount -o rw,nosuid,nodev,relatime,uid=1000,gid=1000,fmask=0022,dmask=0077,codepage=437,iocharset=ascii,shortname=mixed,showexec,utf8,flush,errors=remount-ro,uhelper=udisks2 /dev/sda1 /home/pi/ucard
