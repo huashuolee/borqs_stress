93a6e5b371dd6be2d1cf46c283ae443656fbab92
huashuolee <huashuolee@gmail.com>
2017-02-05 00:53:33 +0800

    mount on onion

--- raspberry/get_cpu_temp.py
+++ raspberry/get_cpu_temp.py
@@ -1,5 +1,5 @@
 
 with open("/sys/class/thermal/thermal_zone0/temp","r") as file:
     temp = float(file.read())/1000
-print "tempture:{} C ".format(temp)
+print ("tempture:{} C ".format(temp))
 
--- raspberry/mountf.sh
+++ raspberry/mountf.sh
@@ -1,1 +1,2 @@
 mount -o rw,nosuid,nodev,relatime,uid=1000,gid=1000,fmask=0022,dmask=0077,codepage=437,iocharset=ascii,shortname=mixed,showexec,utf8,flush,errors=remount-ro,uhelper=udisks2 /dev/sda1 /home/pi/ucard
+# mount -o rw,nosuid,nodev,relatime,uid=1000,gid=1000,fmask=0022,umask=0000,dmask=0077,nls=utf8,errors=remount-ro /dev/sda1 /home/tmp/
