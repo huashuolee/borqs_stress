dc72e5bb35d3bab8b57a7e04b3981671a8f4d5d1
huashuolee <huashuolee@gmail.com>
2017-04-05 13:47:52 +0800

    update 2

--- otherTools/demo_traverse.py
+++ otherTools/demo_traverse.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 import os
 import os.path
-rootdir = "./"                         
+rootdir = "/home/huashuolee/data/tmp"                         
 
 for parent,dirnames,filenames in os.walk(rootdir):    #三个参数：分别返回1.父目录 2.所有文件夹名字（不含路径） 3.所有文件名字
     for dirname in  dirnames:                       #输出文件夹信息
